0ea715deac18dc6c6b4b89c994ab91fd52691151
Hilton Junior <146493108+Devmmx166@users.noreply.github.com>
2023-10-25 17:46:25 -0300

    Calculadora v2.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,67 +1,135 @@
-function criaCalculadora() {
-    return {
-        display: document.querySelector('.display'),
-        btnClean: document.querySelector('.btnClean'),
-        inicia() {
-            this.cliqueBotoes(); 
-           },
+const previusOperationText = document.querySelector("#previus-operations");
+const currentOperationText = document.querySelector("#current-operations");
+const buttons = document.querySelectorAll("#buttons-container button");
 
-           realizaConta(){
-            let conta = this.display.value;
-
-            try{
-                conta = eval(conta);
-                if(!conta){
-                    alert('Conta invalida');
-                    return;
-                }
-                this.display.value = String( conta);
-            }
-            catch(e){
-            alert('Conta Invalida');
+class Calculator{
+    constructor(previusOperationText,currentOperationText){
+     this.previusOperationText = previusOperationText;
+     this.currentOperationText = currentOperationText;
+     this.currentOperation = "";
+    }
+    // add digito na tela da calculadora
+    addDigit(digit){  
+        if(digit === "." && this.currentOperationText.innerText.includes(".") ){
             return;
-            }
-           },
-        cleanDisplay(){
-            this.display.value = " ";
-        },
-        apagaUm(){                               //faz apagar apenas o ultimo dig
-            this.display.value = this.display.value.slice(0, -1);
-        },
-  
-        cliqueBotoes(){
-            // this -> calculadora
-         document.addEventListener('click', function(e){
-const el = e.target;
-console.log(this)
-           if(el.classList.contains('btn-num')){
-            this.btnParaDisplay(el.innerText);
            }
 
-           if(el.classList.contains('btn-clean')){
-            this.cleanDisplay();
-           }
+    this.currentOperation = digit;
+    this.updateScreen();
+     
+    }
 
-           if(el.classList.contains('btn-del')){
-            this.apagaUm();
-           };
-
-           if(el.classList.contains('btn-eq')){
-            this.realizaConta();
-            if(e.key === 'Enter') {
-  
-                var btn = document.querySelector("#submit");
-              
-              btn.click();
-            
+    // processar todos os calculos das operações
+    processOperation(operation){
+        if(this.currentOperationText.innerText === "" && operation !== "C"){
+            if(previusOperationText.innerText !== ""){
+                this.currentOperation(operation);
             }
-           };
-         }.bind(this));
-        },
-        btnParaDisplay(valor) {
-            this.display.value += valor;
         }
-    };
+        
+        // realizar operações
+        let operationValue;
+        const previus= +this.previusOperationText.innerText.split(" ")[0]
+        const current = +this.currentOperationText.innerText;
+
+        switch (operation) {
+            case "+":
+                operationValue = previus + current;
+                this.updateScreen(operationValue, operation, current, previus);
+                break;
+                case "-":
+                    operationValue = previus - current;
+                    this.updateScreen(operationValue, operation, current, previus);
+                    break;
+                    case "/":
+                        operationValue = previus / current;
+                        this.updateScreen(operationValue, operation, current, previus);
+                        break;
+                        case "*":
+                            operationValue = previus * current;
+                            this.updateScreen(operationValue, operation, current, previus);
+                            break;
+                            case "DEL":
+                             this.del()
+                                break;
+                                case "CE":
+                                    this.clear()
+                                       break;
+                                       case "C":
+                                        this.clearOperation()
+                                           break;
+                                           case "=":
+                                        this.processEqualOperator()
+                                           break;
+                default:
+                    return;
+         }
+    }
+  
+
+    updateScreen(
+       operationValue = null,
+       operation = null,
+       current = null,
+       previus = null
+    ){
+       if(operationValue === null){
+               this.currentOperationText.innerText += this.currentOperation;
+
+       }else{
+        if(previus === 0){
+            operationValue = current
+        }
+        // historico do calculo
+this.previusOperationText.innerText = `${operationValue} ${operation}`;
+this.currentOperationText.innerText = "";
+       }
+    }
+//changer operation
+changeOperation(){
+    const mathOperations = ["*", "/", "+" , "-"]
+
+    if(!mathOperations.includes(operation)){
+        return
+    }
+    //123 Operation
+    this.previusOperationText.innerText = this.previusOperationText.innerText.slice(0, -1) + operation;
 }
-const calculadora = criaCalculadora();
-calculadora.inicia()
+
+del(){
+    this.currentOperationText.innerText =
+     this.currentOperationText.innerText.slice(0, -1);
+}
+
+clear(){
+    this.currentOperationText.innerText = "";  
+}
+clearOperation(){
+   this.currentOperationText.innerHTML = "";
+   this.previusOperationText.innerHTML = "";
+
+}
+processEqualOperator(){
+     const operation = previusOperationText.innerText.split(" ")[1];
+     this.processOperation(operation);
+}
+
+}  
+  //
+ 
+
+
+const calc = new Calculator(previusOperationText, currentOperationText);
+
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        const value = e.target.innerText;
+
+        if(+value >= 0 || value === "."){
+           calc.addDigit(value);
+        }else{
+          calc.processOperation(value)
+        }
+    });
+});
